a284c392d6e185403e67a60dea5bc85ffa40fd38
Ppx8 <px8>
2025-10-29 15:46:18 +0900

    update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 function changeText() {
     const h1 = document.getElementById("main-text");
     if(h1.innerText === "Hello, Flask!!") {
-        h1.innerText = "文字が変わりました！！！";
+        h1.innerText = "文字が変わりました???";
     } else {
         h1.innerText = "Hello, Flask!!";
     }
